d0adb89cc5a484709bda026810282dcc4549fd22
Zakir Aliyev <aliyevzakir814@gmail.com>
2025-12-29 21:46:17 +0400

    Initial Commit

---
+++ iosCourse/iosCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosCourseApp.swift
+//  iosCourse
+//
+//  Created by Zakir Aliyev on 29.12.25.
+//
+
+import SwiftUI
+
+@main
+struct iosCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosCourseTests/iosCourseTests.swift
@@ -0,0 +1,16 @@
+//
+//  iosCourseTests.swift
+//  iosCourseTests
+//
+//  Created by Zakir Aliyev on 29.12.25.
+//
+
+import Testing
+
+struct iosCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
